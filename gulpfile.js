7be0da8d0b5b61426f086019376bb482db7a19a4
Andrew Jarrett <ahrjarrett@gmail.com>
2017-02-01 15:05:31 -0700

    added gulp develop and watch tasks, test to make sure autoreload is working though?

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp')
 const autoprefixer = require('gulp-autoprefixer')
 const del = require('del')
+const livereload = require('gulp-livereload')
+const nodemon = require('gulp-nodemon')
 const sass = require('gulp-ruby-sass')
 
 const config = {
@@ -8,9 +10,33 @@ const config = {
   sass: './public/css/**/*.scss'
 }
 
+gulp.task('default', [
+  'styles',
+  'develop',
+  'watch'
+])
+
 gulp.task('clean-styles', () => {
   const files = config.css + '*.css'
   del(files)
+})
+
+gulp.task('develop', () => {
+  livereload.listen()
+
+  nodemon({
+    script: 'bin/www',
+    ext: 'js jade',
+    stdout: false
+  }).on('readable', function() {
+    this.stdout.on('data', function(chunk) {
+      if(/^Express server listening on port/.test(chunk)) {
+        livereload.changed(__dirname)
+      }
+    })
+    this.stdout.pipe(process.stdout)
+    this.stderr.pipe(process.stderr)
+  })
 })
 
 gulp.task('styles', ['clean-styles'], () => {
@@ -19,6 +45,11 @@ gulp.task('styles', ['clean-styles'], () => {
   return sass(config.sass)
     .pipe(autoprefixer({ browsers: ['last 2 version', '> 5%'] }))
     .pipe(gulp.dest(config.css))
+})
+
+gulp.task('watch', () => {
+  gulp.watch(config.sass, ['styles'])
+  //gulp.watch(config.js, ['scripts'])
 })
 
 
