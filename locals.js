ea5ac6a72fa671d5c1fd9d5c45c0983b745fc4d6
Andrew Jarrett <ahrjarrett@gmail.com>
2017-02-02 00:18:02 -0700

    fixed nav styles, next icons

--- locals.js
+++ locals.js
@@ -1,5 +1,6 @@
 module.exports = (app) => {
   app.locals.company = 'HawaiiMIL'
+  app.locals.email = 'shannon@shannonsmithrealtor.com'
   app.locals.phone = '8082253736'
   app.locals.prettify_phone = '(808) 225-3736'
   app.locals.nav = {
