532dcef63029f373ef135078b68ed3921496a9e1
Andrew Jarrett <ahrjarrett@gmail.com>
2017-02-01 23:45:17 -0700

    added styles to navbar

---
+++ locals.js
@@ -0,0 +1,17 @@
+module.exports = (app) => {
+  app.locals.company = 'HawaiiMIL'
+  app.locals.phone = '8082253736'
+  app.locals.prettify_phone = '(808) 225-3736'
+  app.locals.nav = {
+    'Home': '',
+    'VA Home Loans': 'loans',
+    'VA Resources': 'resources',
+    'Buy vs. Rent?': 'buy-vs-rent',
+    'News': 'news'
+  }
+  app.locals.social = {
+    'facebook': 'https://facebook.com/HawaiiMIL',
+    'houzz': 'https://houzz.com/HawaiiMIL'
+  }
+}
+
